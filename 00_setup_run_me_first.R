73535a09fab2ed8b92b973efd78193fa412a97d6
jslingsby <jslingsby@gmail.com>
2022-03-14 08:23:56 +0200

    Added sentinel satellite data analysis

--- 00_setup_run_me_first.R
+++ 00_setup_run_me_first.R
@@ -1,3 +1,5 @@
 ### Setup script to add packages needed to run the prac
 
 install.packages(c("tidyverse", "picante", "readxl", "knitr", "FSA", "hrbrthemes", "sf", "MODISTools"))
+
+update.packages("markdown")
