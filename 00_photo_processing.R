e2902ceab06ab4e43d38b4c81307913ad2610b0c
jslingsby <jslingsby@gmail.com>
2023-01-28 22:17:46 +0200

    ...

--- 00_photo_processing.R
+++ 00_photo_processing.R
@@ -4,6 +4,6 @@ library(readxl)
 sdat <- read_xlsx("data/pracdatasheet.xlsx", sheet = "Species")
 
 splists <- sdat %>% select(Site, FullName) %>% unique() %>% 
-  filter(Site == "renosterveld") %>% arrange(FullName)
+  filter(Site == "invasion") %>% arrange(FullName)
 
 splists_st <- sdat %>% select(Point, Site, FullName) %>% unique()
